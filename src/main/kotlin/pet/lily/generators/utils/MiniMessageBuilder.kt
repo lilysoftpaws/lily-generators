b0b942e967a1f57f1cce98e99d29ea76082bd025
lilyvxv <123564796+lilyvxv@users.noreply.github.com>
2025-01-07 12:33:14 +0000

    docs: add doc for MiniMessageBuilder.kt

--- src/main/kotlin/pet/lily/generators/utils/MiniMessageBuilder.kt
+++ src/main/kotlin/pet/lily/generators/utils/MiniMessageBuilder.kt
@@ -2,6 +2,11 @@ package pet.lily.generators.utils
 
 import net.kyori.adventure.text.minimessage.MiniMessage
 
+/**
+ * Returns a custom MiniMessage builder for global parsing.
+ *
+ * @return A new instance of MiniMessage.Builder.
+ */
 fun getMiniMessageBuilder(): MiniMessage.Builder {
     return MiniMessage.builder()
 }
